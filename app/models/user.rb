82a65ee92a1564e375a6b1c94c2efe0bf1b48be5
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-05-31 14:30:01 -0400

    user accepts access_keys, is more renderable.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
-  # attr_accessible :title, :body
+
   has_many :access_keys, :as => :owner
   has_many :user_sessions
+
+  accepts_nested_attributes_for :access_keys
+  
+  attr_accessible :access_keys_attributes
+  
+  def to_s
+  	name
+  end
   
 end
