719423803a0795bced7d4711c5d99dc339963269
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-05-31 14:31:06 -0400

    application controller can now find current user session.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,26 @@
 class ApplicationController < ActionController::Base
+	
 	protect_from_forgery
+	
+	before_filter :find_current_user_session
 	
 	helper :all
 	helper_method :current_user_session, :current_user
-	filter_parameter_logging :password, :password_confirmation
     
-private    
+private
+
+	def find_current_user_session
+		if session[:user_session_id]
+			@current_user_session = UserSession.where(:id => session[:user_session_id]).first
+		end
+	end
     
 	def current_user_session
-		return @current_user_session if defined?(@current_user_session)
-		@current_user_session = UserSession.find
+		@current_user_session
 	end
 	
 	def current_user
-		return @current_user if defined?(@current_user)
-		@current_user = current_user_session && current_user_session.record
+		current_user_session && current_user_session.user
 	end
 	
 	def require_user
@@ -36,11 +42,11 @@ private
 	end
 	
 	def store_location
-      session[:return_to] = request.request_uri
+    	session[:return_to] = request.request_uri
     end
 
     def redirect_back_or_default(default)
-      redirect_to(session[:return_to] || default)
-      session[:return_to] = nil
+    	redirect_to session[:return_to] || default
+    	session[:return_to] = nil
     end
 end
