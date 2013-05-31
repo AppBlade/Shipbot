2bcda280f8a1e7582ffe06d5a8d746d6862e19fe
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-05-31 14:31:47 -0400

    Removing handle_existing_oauth, not needed. Just redirect to login.

--- app/controllers/oauth_controller.rb
+++ app/controllers/oauth_controller.rb
@@ -3,16 +3,15 @@ class OauthController < ApplicationController
   processes_oauth_transactions_for :access_keys,
                                    :through  => lambda { current_user || User.new },
                                    :callback => lambda { oauth_callback_url },
-                                   :conflict => :handle_existing_oauth,
+                                   :conflict => :handle_oauth_login,
                                    :login    => :handle_oauth_login
 
   def handle_oauth_login(user)
-	 user_session = UserSession.new :user => user
+	 user_session = UserSession.new
+	 user_session.user = user
 	 user_session.save
-  end
-
-  def handle_existing_oauth(user)
-  	redirect_to root_url, :error => "Already linked with another account"
+	 session[:user_session_id] = user_session.id
+	 redirect_to root_url
   end
 
 end
