a9b245ac4915abfd9d34dec9b446dde3d1505337
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-06-03 09:21:28 -0400

    Displaying the time since a user joined in the user settings.

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,5 +1,14 @@
 module UserHelper
 
+ def time_since(date_time)
+	string_frmt = "%l:%M %p today" if date_time.today?
+	string_frmt ||= "%A" if (Time.now - date_time) < 7.days 
+	string_frmt ||= "%b %d" if (Time.now - date_time) < 1.month
+	string_frmt ||= "%m/%d/%Y"
+	return date_time.strftime(string_frmt) unless string_frmt == "%b %d"
+	date_time.strftime(string_frmt).ordinalize
+ end
+
   def main_provider(user)
 	if user.access_keys.count > 0
   		user.access_keys.first.service
