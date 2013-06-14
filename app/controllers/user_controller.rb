7a8f0ba99972f8cb62bc8baab2279777593210e0
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-06-14 14:13:59 -0400

    adding destroy to the user_controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,4 +11,15 @@ class UserController < ApplicationController
 	def edit
 	
 	end
+	
+	def destroy 
+		if current_user.id && params[:confirm_id]
+			User.find(current_user.id).destroy
+	    	session[:user_session_id] = nil
+    		redirect_to root_url
+    	else
+    		redirect_to users_url
+    		flash[:notice] = "You do not have access to do that."
+		end
+	end
 end
