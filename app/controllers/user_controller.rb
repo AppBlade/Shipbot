866abea29dba1ee59d672f01f3d4cfacffd27426
Andrew Tremblay <andrew.tremblay@raizlabs.com>
2013-06-14 14:59:36 -0400

    flash notice in the users_controller#destroy

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -16,10 +16,11 @@ class UserController < ApplicationController
 		if current_user.id && params[:confirm_id]
 			User.find(current_user.id).destroy
 	    	session[:user_session_id] = nil
+    		flash[:notice] = "Account Deleted. Sorry to see you go!"
     		redirect_to root_url
     	else
     		redirect_to users_url
-    		flash[:notice] = "You do not have access to do that."
+    		flash[:error] = "You do not have access to do that."
 		end
 	end
 end
